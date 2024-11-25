a54af800fbaf0f087f3b51d9141b2838841ca161
kinddevs <kinddevs2024@gmail.com>
2024-11-25 20:16:46 +0500

    start

--- src/pages/About/Aboute.jsx
+++ src/pages/About/Aboute.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 import { Spinner } from "@material-tailwind/react";
 
--- src/pages/Eror-404/Eror.jsx
+++ src/pages/Eror-404/Eror.jsx
@@ -1,11 +1,12 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 
 const Eror = () => {
   return (
     <div className='flex justify-center items-center'>
-      <img className='w-[70%] h-[50%]' src="public/404 error with a landscape-bro.svg" alt="404" />
+      <img className='w-[70%] h-[50%]' src="public/404.svg" alt="404" />
     </div>
   )
 }
 
-export default Eror
+export default Eror;
